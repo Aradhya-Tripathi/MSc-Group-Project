ece36ed4657415000be216852e80373e801f9cde
Aradhya Tripathi <67282231+Aradhya-Tripathi@users.noreply.github.com>
2024-03-26 16:36:49 +0530

    feat: Added support for plotting predictions (getting-there)

--- src/af/render/dash_app.py
+++ src/af/render/dash_app.py
@@ -1,12 +1,23 @@
 import dash_bio
-from dash import Input, Output, callback, html
+from dash import html
 from dash_bio.utils import PdbParser, create_mol3d_style
+import dash_bootstrap_components as dbc
 from django_plotly_dash import DjangoDash
 
 app = DjangoDash("AF-Plots")
 
-parser = PdbParser("path/to/pdb")
+parser = PdbParser(
+    "/Users/aradhya/Desktop/Uni-Projects/group-project/out/test_unrelaxed_rank_002_alphafold2_ptm_model_4_seed_000.pdb"
+)
 
+parser_2 = PdbParser(
+    "/Users/aradhya/Desktop/Uni-Projects/group-project/out/test_unrelaxed_rank_004_alphafold2_ptm_model_5_seed_000.pdb"
+)
+
+data_2 = parser.mol3d_data()
+styles_2 = create_mol3d_style(
+    data_2["atoms"], visualization_type="cartoon", color_element="residue"
+)
 data = parser.mol3d_data()
 styles = create_mol3d_style(
     data["atoms"], visualization_type="cartoon", color_element="residue"
@@ -16,31 +27,22 @@ styles = create_mol3d_style(
 app.layout = html.Div(
     [
         dash_bio.Molecule3dViewer(
-            id="dashbio-default-molecule3d", modelData=data, styles=styles
+            id="dashbio-default-molecule3d",
+            modelData=data,
+            styles=styles,
+            style={"width": "50%"},
         ),
-        "Selection data",
-        html.Hr(),
-        html.Div(id="default-molecule3d-output"),
-    ]
+        dash_bio.Molecule3dViewer(
+            id="dashbio-default-molecule3d-2",
+            modelData=data_2,
+            styles=styles_2,
+            style={"width": "50%"},
+        ),
+    ],
+    style={
+        "background": "black",
+        "height": "100vh",
+        "width": "100vw",
+        "display": "flex",
+    },
 )
-
-
-@callback(
-    Output("default-molecule3d-output", "children"),
-    Input("dashbio-default-molecule3d", "selectedAtomIds"),
-)
-def show_selected_atoms(atom_ids):
-    if atom_ids is None or len(atom_ids) == 0:
-        return "No atom has been selected. Click somewhere on the molecular \
-        structure to select an atom."
-    return [
-        html.Div(
-            [
-                html.Div("Element: {}".format(data["atoms"][atm]["elem"])),
-                html.Div("Chain: {}".format(data["atoms"][atm]["chain"])),
-                html.Div("Residue name: {}".format(data["atoms"][atm]["residue_name"])),
-                html.Br(),
-            ]
-        )
-        for atm in atom_ids
-    ]
